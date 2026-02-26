4a8826c7577638fc469a785a730df3ea27ba0a44
Abu Solayman Sefat <sefat01625@gmail.com>
2026-02-26 11:30:10 +0600

    data face compont create button visited for our project

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Country.css'
 
-const Country = () => {
+const Country = ({ country }) => {
+          // console.log(country.population.population)
+
+          const handleVisited = () => {
+                    console.log('visited', country.name.common)
+          }
           return (
-                    <div>
-                           <h4>Country Component</h4>   
+                    <div className='country'>
+                              <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+                              <h4>Country Component: {country.name.common}</h4>
+                              <p>population: {country.population.population}</p>
+                              <p>Area: {country.area.area} {country.area.area > 3000 ? "Large Country" : "Small Country"}</p>
+                              <button onClick={handleVisited}>Mark as visited</button>
                     </div>
           );
 };
