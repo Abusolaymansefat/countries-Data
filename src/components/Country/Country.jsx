b24195d4524395373edd979ab42f663e9f616b3e
Abu Solayman Sefat <sefat01625@gmail.com>
2026-02-27 22:52:30 +0600

    data count for data face

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
+          const [visited, setVisited] = useState(false)
           // console.log(country.population.population)
 
           const handleVisited = () => {
-                    console.log('visited', country.name.common)
+                    // console.log('visited', country.name.common)
+                    // setVisited(true)
+                    // if(visited){
+                    //           setVisited(false)
+                    // }
+                    // else{
+                    //           setVisited(true)
+                    // }
+                    setVisited(!visited)
           }
           return (
-                    <div className='country'>
+                    <div className={`country ${visited ? 'country-visited' : ''}`}>
                               <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
                               <h4>Country Component: {country.name.common}</h4>
                               <p>population: {country.population.population}</p>
                               <p>Area: {country.area.area} {country.area.area > 3000 ? "Large Country" : "Small Country"}</p>
-                              <button onClick={handleVisited}>Mark as visited</button>
+                              <button onClick={handleVisited}>{visited ? "Visited" : "Mark as visited"}</button>
                     </div>
           );
 };
